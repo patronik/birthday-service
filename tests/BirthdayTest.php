6b25dff1ac97ce4e2d7cbb585026d36535021b99
Vasyl Voina <vasyl.voina@regiondo.com>
2021-06-01 13:09:49 +0300

    Tests added

--- tests/BirthdayTest.php
+++ tests/BirthdayTest.php
@@ -5,30 +5,72 @@ use App\BirthdayInfo;
 
 class BirthdayTest extends TestCase
 {
+    public function testAge()
+    {
+        $birthDateDatetime = new \DateTime('now');
+        $birthDateDatetime->modify('-20 years');
+
+        $intervalHelper = new Interval();
+        /** @var BirthdayInfo $birthdayInfo */
+        $birthdayInfo = $intervalHelper->collectInfo($birthDateDatetime, 'Europe/Kiev');
+
+        $this->assertTrue($birthdayInfo->getCurrentAge() == 20);
+    }
+
     public function testBirthdayToday()
     {
-        $intervalHelper = new Interval();
-        $birthDatetime = new \DateTime('now');
-        $birthDatetime->modify('-20 years');
+        $birthDateDatetime = new \DateTime('now');
+        $birthDateDatetime->modify('-20 years');
 
+        $intervalHelper = new Interval();
         /** @var BirthdayInfo $birthdayInfo */
-        $birthdayInfo = $intervalHelper->collectInfo($birthDatetime, 'Europe/Kiev');
-        
+        $birthdayInfo = $intervalHelper->collectInfo($birthDateDatetime, 'Europe/Kiev');
+
         $this->assertTrue($birthdayInfo->getIsBirthdayToday());
-        $this->assertEquals($birthdayInfo->getCurrentAge(), 20);
     }
 
     public function testBirthdayUpcoming()
     {
-        $intervalHelper = new Interval();
-        $birthDatetime = new \DateTime('now');
-        $birthDatetime->modify('-20 years');
-        $birthDatetime->modify('-10 days');
+        $birthDateDatetime = new \DateTime('now');
+        $birthDateDatetime->modify('-20 years');
+        $birthDateDatetime->modify('-10 days');
 
+        $intervalHelper = new Interval();
         /** @var BirthdayInfo $birthdayInfo */
-        $birthdayInfo = $intervalHelper->collectInfo($birthDatetime, 'Europe/Kiev');
+        $birthdayInfo = $intervalHelper->collectInfo($birthDateDatetime, 'Europe/Kiev');
 
         $this->assertFalse($birthdayInfo->getIsBirthdayToday());
-        $this->assertEquals($birthdayInfo->getCurrentAge(), 20);
+    }
+
+    public function testIntervalToUpcoming()
+    {
+        $nowDatetime = new \DateTime('now');
+
+        $nextBirthdayDatetime = clone $nowDatetime;
+        $nextBirthdayDatetime->modify('+20 days');
+
+        $birthDateDatetime = clone $nextBirthdayDatetime;
+        $birthDateDatetime->modify('-20 years');
+
+        $intervalHelper = new Interval();
+        /** @var BirthdayInfo $birthdayInfoFromNow */
+        $birthdayInfoFromNow = $intervalHelper->collectInfo($birthDateDatetime, 'Europe/Kiev');
+        $calculatedNextBirthday = (clone $nowDatetime)->add(
+            $birthdayInfoFromNow->getIntervalToNextBirthday()
+        );
+
+        $this->assertEquals(
+            $calculatedNextBirthday->format('Y-m-d'),
+            $nextBirthdayDatetime->format('Y-m-d')
+        );
+
+        /** @var BirthdayInfo $birthdayInfoFromNow */
+        $birthdayInfoFromTomorrow = $intervalHelper->collectInfo(
+            $birthDateDatetime, 'Europe/Kiev', (clone $nowDatetime)->modify('+1 day'));
+
+        $this->assertTrue(
+            (int) $birthdayInfoFromTomorrow->getIntervalToNextBirthday()->format('%a') <
+            (int) $birthdayInfoFromNow->getIntervalToNextBirthday()->format('%a')
+        );
     }
 }
